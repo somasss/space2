ce3fd4102bd12dabe2f8203dcd11361df174c880
soma <ssyy20210101@gmail.com>
2022-06-17 02:28:16 +0900

    fix layout

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -2,7 +2,7 @@ class Beat < ApplicationRecord
   belongs_to :user
   mount_uploader :audio, AudioUploader
   has_many :beatcategory, dependent: :destroy
-  has_many :categories, through: :beatcategory
+  has_many :categories, through: :beatcategory, dependent: :destroy
   validates :beat_time, presence: true
   validates :beat_title, presence: true
 end
