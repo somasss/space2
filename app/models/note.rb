7c7e4cfd5a353445351813ec3682ab5eb8345aa6
soma <ssyy20210101@gmail.com>
2022-06-20 11:24:48 +0900

    add validations to notes beats users tasks

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,4 +4,7 @@ class Note < ApplicationRecord
   has_many :labels, through: :notelabels
   validates :note_title, presence: true
   validates :text, presence: true
+  validates :user_id, presence: true
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :beats
   has_many :notes
   has_many :tasks
+  validates :email, presence: true
+  validates :admin, presence: true
+  validates :name, presence: true
+  validates :sign_in_count, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
