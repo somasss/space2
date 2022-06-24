eaa2f53eb04f4733bf5cb8e7d3bde1d005b779f0
soma <ssyy20210101@gmail.com>
2022-06-24 21:18:11 +0900

    init

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,14 +6,12 @@ class Users::SessionsController < Devise::SessionsController
     guest_user = User.guest
     sign_in guest_user 
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-    binding.irb
   end
 
   def admin_guest_sign_in
     admin_user = User.admin_guest
     sign_in admin_user
     redirect_to root_path, notice: 'ゲストadminユーザーとしてログインしました。'
-    binding.irb
   end
 
   # GET /resource/sign_in
