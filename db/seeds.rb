92dd2d71a02dff00fc6f03e46d174114741ce060
soma <ssyy20210101@gmail.com>
2022-06-24 19:03:01 +0900

    fix add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,39 @@ Category.create([
     { name: 'R&B' },
     { name: 'techno'}
     ])
+
+5.times do |n|
+    User.create(
+        name: "User#{n}",
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(min_length: 6),
+        )
+end
+
+user = User.create(name: "TestUser",email: Faker::Internet.email, password: Faker::Internet.password(min_length: 6),)
+
+5.times do |n|
+    Beat.create(
+        beat_title: "beat#{n}",
+        beat_time: 10,
+        user_id: user.id,
+        )
+end
+    
+
+5.times do |n|
+    Task.create(
+        task_title: "task#{n}",
+        due_date: "Wed, 29 Jun 2022 00:00:00 UTC +00:00",
+        user_id: user.id,
+        )
+end
+
+5.times do |n|
+    Note.create(
+        note_title: "note#{n}",
+        text: "note#{n}",
+        user_id: user.id,
+        )
+end
+
