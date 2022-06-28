e8585cefbd7970dd2e613fb75eb128cc7bca5465
soma <ssyy20210101@gmail.com>
2022-06-28 16:08:28 +0900

    fix

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -4,15 +4,8 @@ class AudioUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-    CarrierWave.configure do |config|
-      config.cache_storage = :file
-    end
-  else
-    storage :file
-  end
-  # storage :fog
+
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
